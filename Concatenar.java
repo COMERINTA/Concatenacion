feff17863f8ad0eea75400a36559cc55a4eb79f1
COMERINTA <samuel.sam@gmail.com>
2021-12-11 23:06:26 +0100

    Initial commit

---
+++ Concatenar.java
@@ -0,0 +1,14 @@
+package Concatenacion;
+
+public class Concatenar {
+
+    public static void main(String[] args) {
+
+        String[] nombres = {"Samuel","Lizbeth","Claudio","Lucía"};
+
+        for (int i = 0; i < nombres.length; i++) {
+
+            System.out.println(nombres[i]);
+        }
+    }
+}
